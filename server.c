92b4497a2e302912552a6258e699e8fe6541e12e
amirs <>
2002-08-13 11:39:57 +0000

    use setsockopt in server socks

--- server.c
+++ server.c
@@ -85,6 +85,9 @@ void init_server (int port) {
     /* Create an ip socket, bind it, and listen */
     server_sock = socket(PF_INET, SOCK_STREAM, 0);
     check_error(server_sock, "socket");
+    rc = 1;
+    rc = setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (void*)&rc, sizeof(rc));
+    check_error(rc, "setsockopt");
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(port);
     saddr.sin_addr.s_addr = INADDR_ANY;
@@ -97,6 +100,9 @@ void init_server (int port) {
     /* Create an ipv6 socket, bind it, and listen */
     server_sock6 = socket(PF_INET6, SOCK_STREAM, 0);
     check_error(server_sock6, "ipv6: socket");
+    rc = 1;
+    rc = setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (void*)&rc, sizeof(rc));
+    check_error(rc, "ipv6: setsockopt");
     saddr6.sin6_family = AF_INET6;
     saddr6.sin6_port = htons(port);
     saddr6.sin6_addr = in6addr_any;
